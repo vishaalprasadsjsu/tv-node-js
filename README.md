a6fc9dd9ddfa16e7f3815d1096793f981a760167
Vishaal Prasad <vishaal56@gmail.com>
2017-10-31 17:18:21 -0700

    calls TVMaze APIs, populates summary

---
+++ README.md
@@ -0,0 +1,4 @@
+# TV Show Summary Searcher 
+
+Search for a TV show and this Node.js app will search for that TV show's summary using open [TVMaze APIs](http://www.tvmaze.com/api).  
+
--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request');
 const app = express()
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
-	res.send('Hello World!')
+	res.render('index', {tv_summary: null, error: null})
+})
+
+app.post('/', function (req, res) {
+	console.log(req.body.tv_show);
+
+	let url = `http://api.tvmaze.com/search/shows?q=${req.body.tv_show}`
+	request(url, function (err, response, body) {
+    if(err) {
+  		res.render('index', {tv_summary: null, error: 'Error, please try again'});
+    } else {
+		let result = JSON.parse(body)
+		if(result == undefined) {
+			res.render('index', {tv_summary: null, error: 'Error, please try again'});
+
+		} else {
+			let tv_summary_res = `Summary:\n${result[0].show.summary}`;
+			res.render('index', {tv_summary: tv_summary_res, error: null});
+		}
+    }
+  });
 })
 
 app.listen(3000, function () {
